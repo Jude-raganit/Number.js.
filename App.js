414b008d680fe4c3b996ab65215953fb227c482d
Jude-raganit <juderaganit56@gmail.com>
2024-09-10 05:52:49 +0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,24 @@
+const firstfavnum = 18;
+const secondfavnum = 9;
+const sum = firstfavnum + secondfavnum;
+console.log('sum');
+
+const firstfavnum = 18;
+const secondfavnum = 9;
+const different = firstfavnum - secondfavnum;
+console.log('different');
+
+const firstfavnum = 18;
+const  secondfavnum = 9;
+const product = firstfavnum * secondfavnum;
+console.log('product');
+
+const firstfavnum = 18;
+const secondfavnum = 9;
+const percent = firstfavnum % secondfavnum;
+console.log('percent');
+
+const firstfavnum = 18;
+const secondfavnum = 9;
+const exponent = firstfavnum ** secondfavnum;
+console.log('exponent');
